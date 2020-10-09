dbc63a691edfeac4796fd0210d36bbd54fff76c1
Jovan Zoric <jovan@example.com>
2020-10-09 20:12:09 +0200

    errors resolved

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
+const forma = document.querySelector('#form');
 document.querySelector('#toggleButton').addEventListener('click', () => {
-    if (document.querySelector('#form').style.display == "none") {
-        document.querySelector('#form').style.display = "block";
+    if (window.getComputedStyle(forma).display == "none") {
+        forma.style.display = "block";
     } else {
-        document.querySelector('#form').style.display = "none";
+        forma.style.display = "none";
     }
 });
+document.querySelector('#submitButton').addEventListener('click', () => {
+    if (document.querySelector('#title').value == '' || document.querySelector('#content').value == '') {
+        alert('Title i Content su obavezna polja, nemoguce je kreirati post bez njih.');
+    } else {
+        let postTitle = document.createElement('h2');
+        postTitle.innerText = document.querySelector('#title').value;
+        document.querySelector('#mainDiv').appendChild(postTitle);
+
+        let postContent = document.createElement('p');
+        postContent.innerText = document.querySelector('#title').value;
+        document.querySelector('#mainDiv').appendChild(postContent);
+
+        forma.style.display = "none";
+    }
+})
